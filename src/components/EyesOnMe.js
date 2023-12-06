34a6bd2b3a3dfe2311356eb0c6c51958cc5e67a1
Joan <joanmuhingo@gmail.com>
2023-12-06 23:15:59 +0300

    progress

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,17 +1,20 @@
 // Code EyesOnMe Component Here
 import React from 'react';
 
-function EyesOnMe(){
-function handleFocus(){
-    console.log('Good');
-}
+function EyesOnMe() {
+  function handleFocus() {
+    console.log('Good!');
+  }
 
-function handleBlur(){
-    console.log('Hey! Eyes on me.')
-}
-    return(
-        <button type='button' onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
-    )
+  function handleBlur() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  return (
+    <button type='button' onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes On Me
+    </button>
+  );
 }
 
 export default EyesOnMe;
